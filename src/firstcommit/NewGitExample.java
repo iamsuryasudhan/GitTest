b788819385b7df5b51413986bf41277a40164703
suryaks <iamsuryasudhan@gmail.com>
2026-05-28 18:49:07 +0530

    file updated

--- src/firstcommit/NewGitExample.java
+++ src/firstcommit/NewGitExample.java
@@ -2,4 +2,10 @@ package firstcommit;
 
 public class NewGitExample {
 
+	public static void main(String[] args)  {
+		
+		System.out.println("firstGitCommit");
+		System.out.println("updatedcode");
+	}
+	
 }
